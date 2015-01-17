a053850534aa93349a77720fc6ebbcbdd377649c
Siddharth Shukla <siddharthshukla@outlook.com>
2015-01-17 01:08:40 +0100

    Add support for images in listings and modify show page

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,4 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "default.jpg"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
