e5979f052637cff58c7565b9c11938096007a5cd
Siddharth Shukla <siddharthshukla@outlook.com>
2015-01-17 01:33:59 +0100

    Add postgres and rails_12factor gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  #Returns the full title on a per-page basis
+  def full_title(page_title)
+    @location = page_title
+    base_title = "Mixology"
+    if page_title.empty?
+      base_title
+      @location = ""
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
